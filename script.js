846e2344b6cb88f776169de4bd78285400d32c4d
Alex <119153127+Amack2@users.noreply.github.com>
2023-04-28 20:24:15 -0700

    completed cleaning, now starting on array

--- script.js
+++ script.js
@@ -5,6 +5,7 @@ let myLibrary = [];
 
 // DOM Objects
 
+//items for the modal
 const addBookBtn = document.querySelector('.addBtn');
 const addBookModal = document.querySelector('#addBookModal');
 const newBookForm = document.querySelector('.newBookInput');
@@ -13,7 +14,12 @@ const submitBookBtn = document.querySelector('#submitBookBtn');
 const authorInput = document.getElementById('authorInput');
 const titleInput = document.getElementById('titleInput');
 const pageCountInput = document.getElementById('pageCountInput');
+const checkbox = document.querySelector('#RSCheck');
+
+//items in the page
 const bookGrid = document.querySelector('.bookGrid');
+
+// items in the book card:
 
 //this is the constructor:
 function Book(title, author, pageCount, readStatus) {
@@ -39,7 +45,11 @@ addBookToLibrary = () => {
   let author = authorInput.value;
   let title = titleInput.value;
   let pageCount = pageCountInput.value;
-  let readStatus = getReadValue();
+  if (checkbox.checked == true) {
+    readStatus = 'true';
+  } else {
+    readStatus = 'false';
+  }
 
   let newBook = new Book(title, author, pageCount, readStatus);
   myLibrary.push(newBook);
@@ -47,41 +57,49 @@ addBookToLibrary = () => {
   createBookCard(title, author, pageCount, readStatus);
 };
 
-const checkbox = document.querySelector('#RSCheck');
-
-function getReadValue() {
-  const checkbox = document.querySelector('#RSCheck');
-  console.log(checkbox.checked);
-  if (checkbox.checked == true) {
-    return (readStatus = 'true');
-  } else {
-    return (readStatus = 'false');
-  }
-}
-
 function createBookCard(title, author, pageCount, readStatus) {
-  let bookCard = document.createElement('div');
+  const bookCard = document.createElement('div');
+  const bookCardTitle = document.createElement('div');
+  const bookCardAuthor = document.createElement('div');
+  const bookCardPageCount = document.createElement('div');
+  const BCToggleContainer = document.createElement('div');
+  const BCIconContainer = document.createElement('div');
+  const BCIconBook = document.createElement('span');
+  const BCIconShare = document.createElement('span');
+  const BCIconClose = document.createElement('span');
+
   bookCard.classList.add('bookCard');
   bookGrid.appendChild(bookCard);
 
-  let bookCardTitle = document.createElement('div');
   bookCardTitle.classList.add('bookCardTitle');
   bookCard.appendChild(bookCardTitle);
   bookCardTitle.innerHTML = 'Title: ' + title;
 
-  let bookCardAuthor = document.createElement('div');
   bookCardAuthor.classList.add('bookCardAuthor');
   bookCard.appendChild(bookCardAuthor);
   bookCardAuthor.innerHTML = 'Author: ' + author;
 
-  let bookCardPageCount = document.createElement('div');
   bookCardPageCount.classList.add('bookCardPageCount');
   bookCard.appendChild(bookCardPageCount);
   bookCardPageCount.innerHTML = 'Page Count: ' + pageCount;
 
-  const BCToggleContainer = document.createElement('div');
   BCToggleContainer.classList.add('readToggle');
   bookCard.append(BCToggleContainer);
+
+  BCIconContainer.classList.add('iconContainer');
+  bookCard.append(BCIconContainer);
+
+  BCIconBook.classList.add('material-symbols-outlined');
+  BCIconBook.innerHTML = 'Book';
+  BCIconContainer.append(BCIconBook);
+
+  BCIconShare.classList.add('material-symbols-outlined');
+  BCIconShare.innerHTML = 'share';
+  BCIconContainer.append(BCIconShare);
+
+  BCIconClose.classList.add('material-symbols-outlined');
+  BCIconClose.innerHTML = 'close';
+  BCIconContainer.append(BCIconClose);
 
   if (this.readStatus == 'true') {
     BCToggleContainer.classList.add('yes');
@@ -90,26 +108,5 @@ function createBookCard(title, author, pageCount, readStatus) {
   BCToggleContainer.addEventListener('click', () => {
     BCToggleContainer.classList.toggle('yes');
     console.log(title);
-    title = 'no';
-    return;
   });
-
-  let BCIconContainer = document.createElement('div');
-  BCIconContainer.classList.add('iconContainer');
-  bookCard.append(BCIconContainer);
-
-  let BCIconBook = document.createElement('span');
-  BCIconBook.classList.add('material-symbols-outlined');
-  BCIconBook.innerHTML = 'Book';
-  BCIconContainer.append(BCIconBook);
-
-  let BCIconShare = document.createElement('span');
-  BCIconShare.classList.add('material-symbols-outlined');
-  BCIconShare.innerHTML = 'share';
-  BCIconContainer.append(BCIconShare);
-
-  let BCIconClose = document.createElement('span');
-  BCIconClose.classList.add('material-symbols-outlined');
-  BCIconClose.innerHTML = 'close';
-  BCIconContainer.append(BCIconClose);
 }
