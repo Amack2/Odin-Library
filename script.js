b980920c9a0737f511ae688f8cc5f537860a66cb
Alex <119153127+Amack2@users.noreply.github.com>
2023-05-01 09:15:50 -0700

    array set up, cleaned up css

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 // All of your book objects are going to be stored in a simple array, so add a function to the script (not the constructor) that can take user’s input and store the new book objects into an array.
 
-numberOfBooks = 0;
 let myLibrary = [];
 
 // DOM Objects
@@ -41,23 +40,46 @@ submitBookBtn.addEventListener('click', () => {
   addBookToLibrary();
 });
 
-addBookToLibrary = () => {
+const getBookInfo = () => {
   let author = authorInput.value;
   let title = titleInput.value;
   let pageCount = pageCountInput.value;
+
   if (checkbox.checked == true) {
     readStatus = 'true';
   } else {
     readStatus = 'false';
   }
 
-  let newBook = new Book(title, author, pageCount, readStatus);
-  myLibrary.push(newBook);
-
-  createBookCard(title, author, pageCount, readStatus);
+  return new Book(title, author, pageCount, readStatus);
 };
 
-function createBookCard(title, author, pageCount, readStatus) {
+addBookToLibrary = () => {
+  let author = authorInput.value;
+  let title = titleInput.value;
+  let pageCount = pageCountInput.value;
+
+  if (author === '' || title === '' || pageCount === '') {
+    return;
+  } else {
+    const newBook = getBookInfo();
+    myLibrary.push(newBook);
+    updateBookGrid();
+  }
+};
+
+function updateBookGrid() {
+  resetBookGrid();
+  for (let book of myLibrary) {
+    createBookCard(book);
+  }
+}
+
+function resetBookGrid() {
+  bookGrid.innerHTML = '';
+}
+
+const createBookCard = (book) => {
   const bookCard = document.createElement('div');
   const bookCardTitle = document.createElement('div');
   const bookCardAuthor = document.createElement('div');
@@ -73,18 +95,32 @@ function createBookCard(title, author, pageCount, readStatus) {
 
   bookCardTitle.classList.add('bookCardTitle');
   bookCard.appendChild(bookCardTitle);
-  bookCardTitle.innerHTML = 'Title: ' + title;
+  bookCardTitle.innerHTML = 'Title: ' + book.title;
 
   bookCardAuthor.classList.add('bookCardAuthor');
   bookCard.appendChild(bookCardAuthor);
-  bookCardAuthor.innerHTML = 'Author: ' + author;
+  bookCardAuthor.innerHTML = 'Author: ' + book.author;
 
   bookCardPageCount.classList.add('bookCardPageCount');
   bookCard.appendChild(bookCardPageCount);
-  bookCardPageCount.innerHTML = 'Page Count: ' + pageCount;
+  bookCardPageCount.innerHTML = 'Page Count: ' + book.pageCount;
 
   BCToggleContainer.classList.add('readToggle');
   bookCard.append(BCToggleContainer);
+  if (book.readStatus == 'true') {
+    BCToggleContainer.classList.add('true');
+  }
+
+  BCToggleContainer.addEventListener('click', () => {
+    if (book.readStatus == 'true') {
+      book.readStatus = 'false';
+      BCToggleContainer.classList.remove('true');
+    } else if (book.readStatus == 'false') {
+      book.readStatus = 'true';
+      BCToggleContainer.classList.add('true');
+    }
+    updateBookGrid();
+  });
 
   BCIconContainer.classList.add('iconContainer');
   bookCard.append(BCIconContainer);
@@ -101,12 +137,24 @@ function createBookCard(title, author, pageCount, readStatus) {
   BCIconClose.innerHTML = 'close';
   BCIconContainer.append(BCIconClose);
 
-  if (this.readStatus == 'true') {
-    BCToggleContainer.classList.add('yes');
-  }
-
-  BCToggleContainer.addEventListener('click', () => {
-    BCToggleContainer.classList.toggle('yes');
-    console.log(title);
+  BCIconClose.addEventListener('click', () => {
+    console.log(myLibrary.indexOf(book));
+    const id = myLibrary.indexOf(book);
+    const removedBook = myLibrary.splice(id, 1);
+    updateBookGrid();
   });
+};
+
+// used for testing layout
+function addBooks(numberOfBooks) {
+  x = numberOfBooks;
+  for (let i = 1; i <= x; i++) {
+    let title = i;
+    let author = i;
+    let pageCount = i;
+    let readStatus = i;
+    new Book(title, author, pageCount, readStatus);
+    myLibrary.push(Book);
+    updateBookGrid();
+  }
 }
